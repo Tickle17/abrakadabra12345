4035e75899d6aa0b1e132f0e5039bc3b9885757f
vadimfrolov <vadimfrolov@simple-it.pro>
2024-06-10 20:25:17 +0500

    feature: удалил cors

--- backend/src/main/kotlin/simple_it/plugins/Security.kt
+++ backend/src/main/kotlin/simple_it/plugins/Security.kt
@@ -1,8 +1,10 @@
 import io.ktor.server.application.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun Application.configureSecurity() {
-//    install(CORS) {
-//        allowCredentials = true
-//        anyHost()
-//    }
+    install(CORS) {
+        allowCredentials = true
+        anyHost()
+        allowHeaders { true }
+    }
 }
