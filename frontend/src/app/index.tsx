b464ceee76a9589e12f99aaab22fcd8e9e6d84c7
FyodorSologub <141565918+FyodorSologub@users.noreply.github.com>
2024-06-15 16:09:04 +0300

    fixed router

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -10,103 +10,102 @@ import {
   Vacancies,
 } from '@/pages';
 import { createBrowserRouter, Navigate } from 'react-router-dom';
-import { useAuthStore, useProfileStore } from './store';
-import { useFetchUserProfile } from '@/widgets/FetchData/fetchProfile.tsx';
+import { useFetchUserProfile } from '@/widgets/FetchData/fetchProfile';
 import { Statistic } from '@/pages/Business/Statistic';
+import React from 'react';
 
 interface PrivateRouteProps {
   element: React.ComponentType;
+  role?: 'business' | 'users';
 }
 
-// eslint-disable-next-line react/prop-types, react-refresh/only-export-components
-const PrivateRoute: React.FC<PrivateRouteProps> = ({ element: Element }) => {
-  const { isLoggedIn, getLoggedInToken } = useAuthStore();
-  const token = getLoggedInToken();
+// eslint-disable-next-line react-refresh/only-export-components
+const PrivateRoute: React.FC<PrivateRouteProps> = ({
+  element: Element,
+  role,
+}) => {
+  const localStorageRole = localStorage.getItem('role');
   useFetchUserProfile();
-  return isLoggedIn || token ? <Element /> : <Navigate to="/auth" />;
+
+  // not logged in
+  if (localStorageRole !== 'business' && localStorageRole !== 'users') {
+    return <AuthPage />;
+  }
+
+  // universal pages
+  if (role === undefined) {
+    return <Element />;
+  }
+
+  // role for path match with localStorage role or state role
+  if (localStorageRole === role) {
+    return <Element />;
+  }
+
+  // role for path doesn't match with state role and localStorage role
+  if (localStorageRole !== role) {
+    switch (localStorageRole) {
+      case 'users':
+        return <Navigate to="/" />;
+      case 'business':
+        return <Navigate to="/statistic" />;
+    }
+  }
 };
 
-// eslint-disable-next-line react/prop-types, react-refresh/only-export-components
-const UserRoute: React.FC<PrivateRouteProps> = ({ element: Element }) => {
-  const { profileData } = useProfileStore();
-  const role = localStorage.getItem('role');
-  useFetchUserProfile();
-  return profileData.role === 'users' || role === 'users' ? (
-    <PrivateRoute element={Element} />
-  ) : (
-    <Navigate to="/statistic" />
-  );
-};
+// eslint-disable-next-line react-refresh/only-export-components
+const AuthRoute: React.FC = () => {
+  const localStorageRole = localStorage.getItem('role');
 
-// eslint-disable-next-line react/prop-types, react-refresh/only-export-components
-const BusinessRoute: React.FC<PrivateRouteProps> = ({ element: Element }) => {
-  const { profileData } = useProfileStore();
-  const role = localStorage.getItem('role');
-  useFetchUserProfile();
-  return profileData.role === 'business' || role === 'business' ? (
-    <PrivateRoute element={Element} />
-  ) : (
-    <Navigate to="/" />
-  );
-};
+  if (localStorageRole !== 'business' && localStorageRole !== 'users') {
+    return <AuthPage />;
+  }
 
-// eslint-disable-next-line react/prop-types, react-refresh/only-export-components
-const AuthRoute = () => {
-  const { isLoggedIn, getLoggedInToken } = useAuthStore();
-  const { profileData } = useProfileStore();
-  const role = localStorage.getItem('role');
-  const token = getLoggedInToken();
-  useFetchUserProfile();
-  return !isLoggedIn || !token ? (
-    <AuthPage />
-  ) : profileData.role === 'business' || role === 'business' ? (
-    <Navigate to="/statistic" />
-  ) : (
-    <Navigate to="/" />
-  );
+  switch (localStorageRole) {
+    case 'users':
+      return <Navigate to="/" />;
+    case 'business':
+      return <Navigate to="/statistic" />;
+  }
 };
 
 export const router = createBrowserRouter([
-  // auth routes
   {
     path: 'auth',
     element: <AuthRoute />,
     errorElement: <ErrorPage />,
   },
-  // universal routes
   {
     path: 'messages',
-    element: <Chat />,
+    element: <PrivateRoute element={Chat} />,
   },
-  // user routes
   {
     path: '/',
-    element: <UserRoute element={RootPage} />,
+    element: <PrivateRoute element={RootPage} role="users" />,
     errorElement: <ErrorPage />,
   },
   {
     path: 'profile',
-    element: <UserRoute element={Profile} />,
+    element: <PrivateRoute element={Profile} role="users" />,
   },
   {
     path: 'vacancies',
-    element: <UserRoute element={Vacancies} />,
+    element: <PrivateRoute element={Vacancies} role="users" />,
   },
-  //   business routes
   {
     path: 'settings',
-    element: <BusinessRoute element={Settings} />,
+    element: <PrivateRoute element={Settings} role="business" />,
   },
   {
     path: 'create-vacancy',
-    element: <BusinessRoute element={CreateVacancy} />,
+    element: <PrivateRoute element={CreateVacancy} role="business" />,
   },
   {
     path: 'calendar',
-    element: <BusinessRoute element={Calendar} />,
+    element: <PrivateRoute element={Calendar} role="business" />,
   },
   {
     path: 'statistic',
-    element: <BusinessRoute element={Statistic} />,
+    element: <PrivateRoute element={Statistic} role="business" />,
   },
 ]);
