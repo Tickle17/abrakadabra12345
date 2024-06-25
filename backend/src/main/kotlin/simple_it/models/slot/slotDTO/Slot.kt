25944b15630b41f01b63f0eab4724808e553058b
vadimfrolov <vadimfrolov@simple-it.pro>
2024-06-25 18:17:54 +0500

    feature: fix slot

--- backend/src/main/kotlin/simple_it/models/slot/slotDTO/Slot.kt
+++ backend/src/main/kotlin/simple_it/models/slot/slotDTO/Slot.kt
@@ -2,7 +2,6 @@ package simple_it.models.slot.slotDTO
 
 import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
-import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 import simple_it.models.calendar.calendarDTO.VacancyCalendarDTO
 import simple_it.models.enum.DayOfWeek
@@ -14,10 +13,10 @@ object VacancySlot : Table() {
     val id = uuid("id").autoGenerate()
     val slot = integer("slot")
     val free = bool("free")
-    val userId = reference("user_id", Users.id).nullable().uniqueIndex()
+    val userId = uuid("user_id").references(Users.id).nullable()
     val communication = varchar("communication", 255)
     val acceptingByUser = bool("accepting_by_user").default(false)
-    val vacancyId = reference("vacancy_id", Vacancy.id, onDelete = ReferenceOption.SET_NULL)
+    val vacancyId = uuid("vacancy_id").references(Vacancy.id).nullable()
     val dayOfWeek = varchar("day_of_week", length = 99999)
     val date = varchar("date", 99999)
     override val primaryKey = PrimaryKey(id)
