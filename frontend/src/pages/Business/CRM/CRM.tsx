e297aea24f37ac10e403d088a52e7cc165b36feb
vadimfrolov <vadimfrolov@simple-it.pro>
2024-06-25 17:11:04 +0500

    feature: убрал некоректную иконку

--- frontend/src/pages/Business/CRM/CRM.tsx
+++ frontend/src/pages/Business/CRM/CRM.tsx
@@ -33,18 +33,6 @@ const ChadCNComponent: React.FC = () => {
             <Column title="Done" count={3} cards={doneCards} />
             <div className="flex-shrink-0 w-6"></div>
           </div>
-          <a
-            className="fixed bottom-0 right-0 flex items-center justify-center h-8 pl-1 pr-2 mb-6 mr-4 text-blue-100 bg-blue-700 rounded-full shadow-lg hover:bg-blue-800"
-            href="https://twitter.com/lofiui"
-            target="_top"
-          >
-            <div className="flex items-center justify-center w-6 h-6 bg-blue-600 rounded-full">
-              <svg className="w-4 h-4 fill-current" viewBox="0 0 24 24">
-                <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
-              </svg>
-            </div>
-            <span className="ml-1 text-sm leading-none">@lofiui</span>
-          </a>
         </div>
       </div>
     </AppLayout>
